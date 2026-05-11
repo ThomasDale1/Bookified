9a491b469c4db3e64cd06786963b7f6964c4fb7f
ThomasDale1 <dalethomas1212@gmail.com>
2026-05-11 15:31:05 -0600

    added image vercel blob hostname to next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {protocol: 'https', hostname: 'covers.openlibrary.org'}
+      {protocol: 'https', hostname: 'covers.openlibrary.org'},
+      {protocol: 'https', hostname: 'd83mbhobygyxucra.public.blob.vercel-storage.com'}
     ]
   }
 };
